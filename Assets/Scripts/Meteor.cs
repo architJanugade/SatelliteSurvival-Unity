1b207ee9a2ab278d04756766c3d19a6d6b5cc7ca
johnnyboiii3020 <48153450+johnnyboiii3020@users.noreply.github.com>
2020-01-13 12:42:06 +0530

    Update Meteor.cs
    Using Triggers

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Meteor : GravityBody
 {
-    public BoxCollider BoxCol;
+    public SphereCollider SphereCol;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
-        BoxCol.enabled = false;
         this.enabled = false; 
         GetComponent<GravityBody>().enabled = false;
+        SphereCol.isTrigger = true;
         Invoke("DestroyMe", 4f);
     }
     public void DestroyMe()
     {
-        BoxCol.enabled = true;
         this.enabled = true;
         GetComponent<GravityBody>().enabled = true;
+        SphereCol.isTrigger = false;    
         ObjectPooler.sharedInstace.BacktoList(gameObject);
         
 
